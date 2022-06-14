c14cbd2316c2e5c81b7505fcb0aab6ff0a99eca0
Sarvang Dave <sarvang00@gmail.com>
2022-06-14 15:35:00 +0000

    Basic webpage html scraper;

--- main.go
+++ main.go
@@ -3,8 +3,24 @@ package main
 import (
 	gvars "GoldenFlask/gvars"
 	"fmt"
+	"io"
+	"net/http"
 )
 
 func main() {
-	fmt.Println(gvars.Site)
+	resp, err := http.Get(gvars.Site)
+	checkErr(err)
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	checkErr(err)
+	content := string(body)
+
+	fmt.Println(content)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
 }
