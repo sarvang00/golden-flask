77574fd569ebc56bf2a31324ff420af1285fd05f
Sarvang Dave <sarvang00@gmail.com>
2022-06-16 09:08:21 +0000

    Added download fn; setup download method [WIP]

--- audiobook/audiobook.go
+++ audiobook/audiobook.go
@@ -1,9 +1,72 @@
 package audiobook
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+)
+
 type AudioBook struct {
 	BookName  string
 	Author    string
 	Reader    string
 	AudioUrl  string
 	StorePath string //set the value after download -> BookName-Author/Reader/
+}
+
+// Function to download an audiobook
+func (ab *AudioBook) DownloadAudiobook() {
+	audioBooks := []string{}
+	downloadPath := "./"
+	if ab.Reader != "" {
+		downloadPath = ab.BookName + "-" + ab.Author + "/" + ab.Reader
+	} else {
+		downloadPath = ab.BookName + "-" + ab.Author
+	}
+
+	// Step-1: Loop through paginator; end in case of error
+
+	// Step-2: Find and loop through to find mp3 urls; add them to an array of strings
+
+	// Step-3: Download mp3 files at a location (BookName-Author/Reader); update StorePath with location
+	DownloadAudios(audioBooks, downloadPath)
+	ab.StorePath = downloadPath
+}
+
+// Function to download audioclips to a specified folder
+func DownloadAudios(urls []string, downloadLocation string) {
+	var wg sync.WaitGroup
+	wg.Add(len(urls))
+
+	for iter, url := range urls {
+		go func(url string) {
+			defer wg.Done()
+			fileName := fmt.Sprintf("%s/chapter%d.mp3", downloadLocation, iter+1)
+			fmt.Println("Downloading", url, "to", fileName)
+
+			output, err := os.Create(fileName)
+			if err != nil {
+				log.Fatal("Error while creating", fileName, "-", err)
+			}
+			defer output.Close()
+
+			res, err := http.Get(url)
+			if err != nil {
+				log.Fatal("http get error: ", err)
+			} else {
+				defer res.Body.Close()
+				_, err = io.Copy(output, res.Body)
+				if err != nil {
+					log.Fatal("Error while downloading", url, "-", err)
+				} else {
+					fmt.Println("Downloaded", fileName)
+				}
+			}
+		}(url)
+	}
+
+	wg.Wait()
 }
