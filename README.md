128154209891f1aa983d604b91c85e6dbcdc6d07
Sarvang Dave <sarvang00@gmail.com>
2022-06-16 07:28:14 +0000

    Started with code restructring

--- README.md
+++ README.md
@@ -7,3 +7,25 @@ Lets make a mini-project in golang
 Amendment: We donot plan to scrap to full site and perform mapping (maybe later?). 
 Instead, we will keep the url such that we will just parse through the url input given
 in CLI and store it at the location specified.
+
+## Excluded file
+The module gvars present in ./ is excluded. It has a struct and the values stored in struct.
+
+The struct was:
+```
+type AudioBook struct {
+	BookName  string
+	Author    string
+	Reader    string
+	HomeUrl   string
+	StorePath string
+}
+```
+
+The variable ```AudioBooksDatabase``` is an array of AudioBook struct. This may be restructured later for better visibility and understanding.
+
+## Credits
+Credit to [nevermosby](https://gist.github.com/nevermosby) for creating code to [concurrently download content](https://gist.github.com/nevermosby/b54d473ea9153bb75eebd14d8d816544) from given URL. It was quite helpful.
+
+## Notice/ Disclaimer
+Use of this or any of the derived codes is up for private use and the writer/publisher of this code shall not be held liable or responsible for the way any underlying concepts or parts of code in full or as whole are used.
--- main.go
+++ main.go
@@ -3,53 +3,15 @@ package main
 import (
 	gvars "GoldenFlask/gvars"
 	"fmt"
-	"io"
-	"net/http"
-	"regexp"
 )
 
 func main() {
-	resp, err := http.Get(gvars.Site)
-	checkErr(err)
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	checkErr(err)
-	content := string(body)
-
-	// fmt.Println(content)
-	parsedTagData := getAnchorTags(content)
-	for tagData := range parsedTagData {
-		fmt.Println(parsedTagData[tagData])
-	}
+	fmt.Println("Scraping for AudioBooks in given Urls")
+	fmt.Println(gvars.AudioBooksDatabase)
 }
 
 func checkErr(err error) {
 	if err != nil {
 		fmt.Println(err)
 	}
-}
-
-func getAnchorTags(httpStringContent string) []string {
-	regexPattern := "<a.*>.*</a>"
-	re := regexp.MustCompile(regexPattern)
-	matchedTags := re.FindAllString(httpStringContent, -1)
-	matchedTags = removeDuplicateStrings(matchedTags)
-	return matchedTags
-}
-
-func removeDuplicateStrings(anchors []string) []string {
-	keys := make(map[string]bool)
-	list := []string{}
-
-	// If the key(values of the slice) is not equal
-	// to the already present value in new slice (list)
-	// then we append it. else we jump on another element.
-	for _, entry := range anchors {
-		if _, value := keys[entry]; !value {
-			keys[entry] = true
-			list = append(list, entry)
-		}
-	}
-	return list
 }
